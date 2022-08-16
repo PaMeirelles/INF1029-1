5508684a7da2e625d42c918138cca558c4056e9d
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-08-16 14:40:50 +0000

    QoL matrix features (print and create)

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix_lib.h"
+
 int main(){
+  float rows[4] = {1.0, 0.0, 0.0, 1.0};
+  s_matrix * a = create_matrix(2, 2, rows);
+  print_matrix(a);
   return 0;
 }
