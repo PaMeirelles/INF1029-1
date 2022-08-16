204f59fe2f998f1ca5ecb077b21f3a3dc4cf3f9f
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-08-16 17:17:53 +0000

    matrix multiplication

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 #include "matrix_lib.h"
 
 int main(){
-  float rows[4] = {1.0, 0.0, 0.0, 1.0};
+  float rows[4] = {2, 0, 1, 9};
+  float rows_2[4] = {3, 9, 4, 7};
+  float rows_3[4] = {1,1,1,1};
+  
   s_matrix * a = create_matrix(2, 2, rows);
+  s_matrix * b = create_matrix(2, 2, rows_2);
+  s_matrix * c = create_matrix(2, 2, rows_3);
+  
+  preenche_com_zero(c);
+
+  matrix_matrix_mult(a, b, c);
+  
   print_matrix(a);
+  print_matrix(b);
+  print_matrix(c);
   return 0;
 }
