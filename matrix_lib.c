29881684661ec4380f006cd708cdd8fe2832cde8
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-08-16 14:40:50 +0000

    QoL matrix features (print and create)

---
+++ matrix_lib.c
@@ -0,0 +1,36 @@
+#include "matrix_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
+  if(matrix == NULL){
+    return 1;
+  }
+  int size = matrix->width * matrix->height;
+  for(int i=0; i < size; i++){
+    matrix->rows[i] = matrix->rows[i] * scalar_value;
+  }
+  return 0;
+}
+
+void print_matrix(s_matrix * matrix){
+  int size = matrix->width * matrix->height;
+  for(int i=0; i < size; i++){
+    printf("%.2f ", matrix->rows[i]);
+    if((i-1) % matrix->height == 0){
+      printf("\n");
+    }
+  }
+}
+
+s_matrix * create_matrix(int width, int height, float * rows){
+  s_matrix * matrix = malloc(sizeof(s_matrix));
+  matrix->width = width;
+  matrix->height = height;
+  matrix->rows = malloc(sizeof(float) * width * height);
+
+  for(int i=0; i < width * height; i++){
+    matrix->rows[i] = rows[i];
+  }
+  return matrix;
+}
--- matrix_lib.h
+++ matrix_lib.h
@@ -2,4 +2,7 @@ struct matrix {
 unsigned long int height;
 unsigned long int width;
 float *rows;
-}
+} typedef s_matrix;
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
+void print_matrix(s_matrix * matrix);
+s_matrix * create_matrix(int width, int height, float * rows);
