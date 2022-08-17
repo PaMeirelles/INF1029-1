4438e776a8881e1a5c742b137eca9d830077f041
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-08-17 13:54:53 +0000

    random matrix generator(for testing)

--- matrix_lib_test.c
+++ matrix_lib_test.c
@@ -1,9 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "matrix_lib.h"
 
-int main(int argc, char * argv[]){
-  double scalar = atof(argv[0]);
-  int dim_a = atoi(argv[1]);
-  int dim_b = atoi(argv[2]);
-}
